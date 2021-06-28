607ad9c2087ebe0f117d8492c2e72313afdeebd6
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-28 22:53:38 +0530

    [Kumar Shubham]Welcome to the Indian States Census Analyser

---
+++ IndianStatesCensusAnalyser/IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Welcome to the Indian States Census Analyser****");
+        }
+    }
+}
